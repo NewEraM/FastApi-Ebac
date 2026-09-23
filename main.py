acbfb1997536d844280ad5d40df664c33ed6b092
muniz <muniz_157@outlook.com>
2026-09-23 20:15:51 -0300

    Adicionando ordenacao em nossa livraria

--- main.py
+++ main.py
@@ -76,12 +76,14 @@ def get_livros(page: int = 1, limit: int = 10,  credentials: HTTPBasicCredential
     if not dicionario_livros:
         return {"message":"Nao existe nenhum livro!!"}
 
+    livros_ordenados = sorted(dicionario_livros.items(), key =lambda x: x[0])
+    
     start = (page - 1)  * limit
     end = start + limit
 
     livros_paginados = [
         {"id": id_livro, "nome_livro": livro_data["nome_livro"], "autor_livro": livro_data["autor_livro"], "ano_livro": livro_data["ano_livro"]}
-        for id_livro, livro_data in list(dicionario_livros.items())[start:end]
+        for id_livro, livro_data in livros_ordenados[start:end]
     ]
 
     return [
